b49cd4c8179d90f8b2259640bf9a54f160b24d57
Nicole Rego <nicole.rego@live.com>
2015-02-24 16:40:34 -0500

    Refactor AJAX search to use $.get method

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -4,32 +4,10 @@ $(document).ready(function(){
 		event.preventDefault();
 		var searchValue = $('#search').val();
 
-		$.ajax({
-			url: '/projects?search=' + searchValue,
-			type: 'GET',
-			dataType: 'html'
-		}).done(function(data){
+		$.get('/projects?search=' + searchValue)
+		.done(function(data){
+			console.log(data);
 			$('.projects').html(data);
 		});
 	});
 });
-
-// Ajax search using the XHR object
-// function display_search_results() {
-// 		if (this.readyState === 4) {
-// 			console.log(this);
-// 			document.getElementsByClassName('projects').innerHTML = this.responseText;
-// 		}
-// 	}
-	
-// var form = document.getElementById('search-form');
-// form.addEventListener('submit', function(event) {
-// 	event.preventDefault();
-// 	var searchValue = document.getElementById('search').value;
-
-// 	var xhr = new XMLHttpRequest();
-// 	xhr.onload = display_search_results;
-// 	xhr.open('GET', '/projects?search=' + searchValue, true);
-// 	xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-// 	xhr.send();
-// });
