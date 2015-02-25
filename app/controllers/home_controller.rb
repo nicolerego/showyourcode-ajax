36a1a0b752300d2346fb732c2fd22b5628ea4dda
Nicole Rego <nicole.rego@live.com>
2015-02-25 12:25:06 -0500

    Limiting home to display 3 most recent, editing render for views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
 	def index
-	 @projects = Project.all
+	 # @projects = Project.all
 		# @projects.recently_added_projects = Project.find(params[:id])
 		# @projects.recently_added_projects = Project.all
+	
+	@projects = Project.order(created_at: :DESC).limit(3)
 	end 
 end
