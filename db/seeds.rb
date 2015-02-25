984f0f9a48ab51c5a3165071557c813c0fd8a933
Nicole Rego <nicole.rego@live.com>
2015-02-25 12:44:52 -0500

    Adding Kaminari gem and updating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,49 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Project.create!(
-	:name => "Project One",
-	:description => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-	:link => "wwww.example.com", 
-	:owner_id => 1
+
+
+# Project.create!(
+# 	:name => "Project One",
+# 	:description => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+# 	:link => "wwww.example.com", 
+# 	:owner_id => 1
+# )
+
+# Project.create!(
+# 	:name => "Project Two",
+# 	:description => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+# 	:link => "wwww.example.com", 
+# 	:owner_id => 1
+# )
+
+# Project.create!(
+# 	:name => "Project Three",
+# 	:description => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+# 	:link => "wwww.example.com", 
+# 	:owner_id => 1
+# )
+
+User.destroy_all
+Project.destroy_all
+
+User.create!({
+  email: "John@bitmakerlabs.com",
+  name: "John Bob",
+  password: "password",
+  password_confirmation: "password"
+  }
 )
 
-Project.create!(
-	:name => "Project Two",
-	:description => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-	:link => "wwww.example.com", 
-	:owner_id => 1
-)
+30.times do |i|
 
-Project.create!(
-	:name => "Project Three",
-	:description => "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-	:link => "wwww.example.com", 
-	:owner_id => 1
+Project.create({
+  name: "Project #{i}",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  link: "www.example.com",
+  owner_id: 1
+  }
 )
+end
+
+
