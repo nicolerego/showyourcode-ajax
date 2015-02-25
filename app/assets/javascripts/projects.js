8b7a76b94ae2d1db36bc71367fd6741d1e54afcb
Nicole Rego <nicole.rego@live.com>
2015-02-25 16:08:27 -0500

    Resolving turbolinks issue

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).on('ready page:load', function(){
 	$('#search-form').submit(function(event){
 		event.preventDefault();
 		var searchValue = $('#search').val();
